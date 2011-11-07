38eecbca8ab95b9cc97629cfcf09584a832224d3
Adam Gerthel <adam.gerthel@oddhill.se>
2011-11-07 16:12:44 +0100

    initial commit
    Signed-off-by: Adam Gerthel <adam.gerthel@oddhill.se>

---
+++ template.php
@@ -0,0 +1,90 @@
+<?php
+/**  
+ * Uncomment if you want to enable apple touch icons.
+ * Create icons in sizes 57x57, 72x72 and 114x114
+ */
+/* <-- REMOVE THIS LINE -->
+// iPhone.
+$icons['iphone_icon'] = array(
+  'href' => drupal_get_path('theme','ODDSITE') . '/apple-touch-icon-iphone.png',
+  'rel' => 'apple-touch-icon',
+);
+// iPad.
+$icons['ipad_icon'] = array(
+  'href' => drupal_get_path('theme','ODDSITE') . '/apple-touch-icon-ipad.png',
+  'rel' => 'apple-touch-icon',
+  'sizes' => '72x72',
+);
+// iPhone 4.
+$icons['iphone4_icon'] = array(
+  'href' => drupal_get_path('theme','ODDSITE') . '/apple-touch-icon-iphone4.png',
+  'rel' => 'apple-touch-icon',
+  'sizes' => '114x114',
+);
+// Add the icons to <head>.
+foreach ($icons as $key => $icon) {
+  $element = array(
+    '#tag' => 'link',
+    '#attributes' => array(
+      'href' => $icon['href'], 
+      'rel' => $icon['rel'],
+      'sizes' => $icon['sizes'],
+    ),
+  );
+  drupal_add_html_head($element, $key);
+}
+*/ //<-- REMOVE THIS LINE -->
+
+/**
+ * Implements hook_css_alter().
+ * This keeps system and module css files from being
+ * rendered by drupal.
+ *
+ * Omitted:
+ * - color.css
+ * - contextual.css
+ * - dashboard.css
+ * - field_ui.css
+ * - image.css
+ * - locale.css
+ * - shortcut.css
+ * - simpletest.css
+ * - toolbar.css
+ */
+function origin_css_alter(&$css) {
+  $exclude = array(
+    'modules/system/system.menus.css' => FALSE,
+    'modules/system/system.messages.css' => FALSE,
+    'modules/system/system.theme.css' => FALSE,
+  );
+  $css = array_diff_key($css, $exclude);
+}
+
+/**
+ * Implements template_preprocess_node().
+ */
+function origin_preprocess_node(&$variables) {
+  // Add the view mode to the template suggestions.
+  $variables['theme_hook_suggestions'][] = 'node__' . $variables['type'] . '__' . $variables['view_mode'];
+}
+
+/**
+ * Implements template_preprocess_field(),
+ */
+function origin_preprocess_field(&$variables, $hook) {
+  // Add the view mode to the template suggestions.
+  $variables['theme_hook_suggestions'][] = 'field__' . $variables['element']['#field_name'] . '__' . $variables['element']['#view_mode'];
+}
+
+/**
+ * Implements template_preprocess_block(),
+ */
+function origin_preprocess_block(&$variables) {  
+  // Alter the title for some blocks.
+  // Manually configured here, because Features doesn't export block configurations.
+  switch ($variables['block']->bid) {
+    case 'system-main-menu':
+      $variables['block']->subject = '';
+      break;
+  }
+}
