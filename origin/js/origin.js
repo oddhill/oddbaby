c1945143343ea88b157497d645d753026f1ae995
Daniel <daniel.friis@oddhill.se>
2011-11-28 08:20:07 +0100

    Major overhaul of entire theme

--- origin/js/origin.js
+++ origin/js/origin.js
@@ -1,9 +1,15 @@
 (function($) {
-	
+
+// Run whenever the DOM tree is changed, e.g. through AJAX/AHAH
 Drupal.behaviors.ORIGIN = {
   attach: function (context, settings) {
   
   }
+}
+
+// Run once when the DOM is ready (page load)
+$(document).ready(function() {
+
 }
 
 })(jQuery);
