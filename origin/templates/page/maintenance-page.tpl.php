8f8c8d8e4c2f8ee9ff0a51da33ec4ac37eb8e863
Adam Gerthel <adam.gerthel@oddhill.se>
2012-09-19 17:47:35 +0200

    Added charset to maintenance page, to prevent odd characters from appearing
    Signed-off-by: Adam Gerthel <adam.gerthel@oddhill.se>

--- origin/templates/page/maintenance-page.tpl.php
+++ origin/templates/page/maintenance-page.tpl.php
@@ -15,7 +15,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
-
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <head>
   <title><?php print $head_title; ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
