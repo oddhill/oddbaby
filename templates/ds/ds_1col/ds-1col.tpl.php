71e2da5ea0b188ca2cc37eea6f1382f925d82314
Olof Johansson <olof.johansson@oddhill.se>
2014-09-23 18:45:06 +0200

    Render contextual links automatically for any entity.
    - Add a variable that will use the existing contextual links if they've been set, or the tabs for the current page if they haven't been set.
    - Render the new variable for the default DS template file.

--- templates/ds/ds_1col/ds-1col.tpl.php
+++ templates/ds/ds_1col/ds-1col.tpl.php
@@ -12,9 +12,6 @@
  */
 ?>
 <div class="<?php print $classes; ?> <?php print $ds_content_classes; ?> contextual-links-region">
-  <?php if (isset($title_suffix['contextual_links'])): ?>
-    <?php print render($title_suffix['contextual_links']); ?>
-  <?php endif; ?>
-
+  <?php print render($contextual_links); ?>
   <?php print $ds_content; ?>
 </div>
