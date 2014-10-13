1082221657b618d6eefbe589ea064cede3b3d2c8
Olof Johansson <olof.johansson@oddhill.se>
2014-10-13 13:33:50 +0200

    Added comments for contextual links rendering.

--- templates/ds/ds_1col/ds-1col.tpl.php
+++ templates/ds/ds_1col/ds-1col.tpl.php
@@ -9,6 +9,14 @@
  * this implementation.
  *
  * Have a look at the example_layout folder in the DS folder for guidance.
+ *
+ * In order to print contextual links for this entity, there are two things
+ * are required:
+ * - Add the contextual-links-region class to the wrapper where the links
+ *   should appear.
+ * - Render the $contextual_links variable within this wrapper.
+ *
+ * @see oddbaby_preprocess()
  */
 ?>
 <div class="<?php print $classes; ?> <?php print $ds_content_classes; ?> contextual-links-region">
