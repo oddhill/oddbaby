34d1a821aa938077df59e6c21aff41558c243f41
Anton Ranestam <anton.ranestam@oddhill.se>
2015-08-13 15:02:17 +0200

    Fixed HTML5 doctype :cat2:

--- templates/defaults/system/html.tpl.php
+++ templates/defaults/system/html.tpl.php
@@ -14,9 +14,8 @@
  * @see template_preprocess_html()
  * @see template_process()
  */
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
-  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-<html xmlns:fb="http://ogp.me/ns/fb#" prefix="og: http://ogp.me/ns#" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+?><!DOCTYPE html>
+<html xmlns:fb="http://ogp.me/ns/fb#" prefix="og: http://ogp.me/ns#" xml:lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
   <head profile="<?php print $grddl_profile; ?>">
     <?php print $head; ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
